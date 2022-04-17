d269a8afed6a46f679fb60894bbe354184ceef31
Whyeon728 <80697051+Whyeon728@users.noreply.github.com>
2022-04-17 16:42:46 +0900

    Initial Commit

---
+++ COVID19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by Whyeon on 2022/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
